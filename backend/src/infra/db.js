414d0a62ec37b8bef57e78fb742c508997785992
utkarsh0z0n3 <utksaxena44@gmail.com>
2026-02-25 22:41:30 +0530

    db.js updated for railwawy deployment

--- backend/src/infra/db.js
+++ backend/src/infra/db.js
@@ -1,16 +1,16 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 export default sequelize;
